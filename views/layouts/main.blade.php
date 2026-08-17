299df41afc33c6dc165dc4a97ada697f35de3385
Aimeos <aimeos@aimeos.org>
2026-08-17 19:48:05 +0200

    Remove header login link

--- views/layouts/main.blade.php
+++ views/layouts/main.blade.php
@@ -172,15 +172,6 @@
                             @endif
                         </li>
                     @endforeach
-                    @if(Route::has('login'))
-                        <li class="login">
-                            <a href="{{ route('login') }}" title="{{ __('Login') }}" aria-label="{{ __('Login') }}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                                    <path d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z" />
-                                </svg>
-                            </a>
-                        </li>
-                    @endif
                 </ul>
                 <ul class="menu-open show">
                     <li>
